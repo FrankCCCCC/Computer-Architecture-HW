edf2d6244ab41e2dd975ad7377549852433b08e3
FrankCCCCC <unaxultraspaceos5@gmail.com>
2019-10-15 02:30:03 +0800

    modify p2

--- arch_hw3_p2_106033233.asm
+++ arch_hw3_p2_106033233.asm
@@ -3,6 +3,10 @@ inputA:		.asciiz "input a: "
 inputB:		.asciiz "input b: "
 ans:		.asciiz "ans: "
 newline:	.asciiz "\n"
+cond0:		.asciiz "cond0 x <= 0"
+cond1:		.asciiz "cond1 y <= 0"
+cond2:		.asciiz "cond2 x > y"
+cond3:		.asciiz "cond3 else fn recursive"
 .text
 
 # Scan input a variable
@@ -25,6 +29,9 @@ li $v0, 5
 syscall
 addi $s1, $v0, 0
 
+# Test re functiom
+
+# Test Fn functiom
 addi $a0, $s0, 0
 addi $a1, $s1, 0
 jal Fn
@@ -48,10 +55,10 @@ sw $a0, 4($sp) #Save int x, $a0
 sw $ra, 0($sp) #Save return address, $ra
 
 slt $t0, $zero, $a0
-beq $t0, 1, Cond0
+beq $t0, 0, Cond0
 
 slt $t0, $zero, $a1
-beq $t0, 1, Cond1
+beq $t0, 0, Cond1
 
 slt $0, $a1, $a0
 beq $t0, 1, Cond2
@@ -60,17 +67,29 @@ j Cond3
 
 Cond0:
 	addi $v1, $zero, 0
+	la $a0, cond0
+	li $v0, 4
+	syscall
 	j Return
 
 Cond1:
 	addi $v1, $zero, 0
+	la $a0, cond1
+	li $v0, 4
+	syscall
 	j Return
 
 Cond2:
 	addi $v1, $zero, 2
+	la $a0, cond2
+	li $v0, 4
+	syscall
 	j Return
 
 Cond3:
+	la $a0, cond3
+	li $v0, 4
+	syscall
 # 3 * fn(x - 1, y)
 addi $a0, $a0, -1
 jal Fn
@@ -118,7 +137,52 @@ addi $sp, $sp, 24 # Delete Stack
 jr $ra
 
 Re:
-addi $t1, $zero, 2
+	addi $sp, $sp, -12
+	sw $s1, 8($sp) # Save $s1, as Re(x - 2)
+	sw $s0, 4($sp) # Save $s0, as Re(x - 1)
+	sw $a0, 0($sp) # Save $a0 as int x
+	addi $t1, $zero, 2
+	slt $t0, $a0, $t1
+	beq $t0, 1, re_cond1
+	
+	re_cond0:
+		# Calculate Re(n-1)
+		addi $a0, $a0, -1
+		jal Re
+		lw $a0, 0($sp)
+		addi $s0, $v1, 0
+		sw $s0, 4($sp)
 
-
+		# Calculate Re(n-2)
+		addi $a0, $a0, -2
+		jal Re
+		lw $a0, 0($sp)
+		addi $s0, $v1, 0
+		sw $s1, 8($sp)
+		
+		lw $s0, 4($sp)
+		lw $s1, 8($sp)
+		mul $t1, $a0, $a0 # $t1 = x * x
+		mul $t2, $a0, $s0 # $t2 = x * re(x - 1)
+		add $t3, $t3, -1  # $t3 = (x - 1)
+		mul $t3, $t3, $s1 # $t3 = (x - 1) * re(x - 2)
+		
+		add $v1, $t1, $t2
+		add $v1, $v1, $t3
+		
+		j re_return
+		
+	re_cond1:
+		bne $a0, 1, re_cond11
+			re_cond10:
+				addi $v1, $zero, 1
+			re_cond11:
+				addi $v1, $zero, 0
+			j re_return
+	re_return:
+	lw $s1, 8($sp) # Save $s1, as Re(x - 2)
+	lw $s0, 4($sp) # Save $s0, as Re(x - 1)
+	lw $a0, 0($sp) # Save $a0 as int x
+	addi $sp, $sp, 12
+	jr $ra
 Exit:
